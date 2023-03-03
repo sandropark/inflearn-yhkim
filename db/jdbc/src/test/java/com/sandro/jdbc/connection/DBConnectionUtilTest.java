a54fb85fbda0657ddb6371f41b7a91a2ff1d2c4a
Sandro Park <ticoba@naver.com>
2023-03-03 21:05:15 +0900

    DB 접근 기술 Init

---
+++ db/jdbc/src/test/java/com/sandro/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,17 @@
+package com.sandro.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class DBConnectionUtilTest {
+    @Test
+    void connection() throws Exception {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+    }
+}
