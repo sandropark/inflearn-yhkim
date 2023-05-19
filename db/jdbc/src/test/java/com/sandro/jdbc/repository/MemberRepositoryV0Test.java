6ee555cccf138f835c9f3df2c3d919f5ad5f2e37
sandropark <ticoba@naver.com>
2023-05-19 17:42:31 +0900

    스프링 DB 접근 1 - 수정, 삭제 구현 및 테스트

--- db/jdbc/src/test/java/com/sandro/jdbc/repository/MemberRepositoryV0Test.java
+++ db/jdbc/src/test/java/com/sandro/jdbc/repository/MemberRepositoryV0Test.java
@@ -3,7 +3,10 @@ package com.sandro.jdbc.repository;
 import com.sandro.jdbc.domain.Member;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MemberRepositoryV0Test {
 
@@ -11,12 +14,28 @@ class MemberRepositoryV0Test {
 
     @Test
     void crud() throws Exception {
+        // save
         Member member = new Member("memberV1", 10000);
         repository.save(member);
 
+        // findById
         Member foundMember = repository.findById(member.getMemberId());
 
         assertThat(foundMember).isEqualTo(member);
+
+        // Update
+        int updatedMoney = 20000;
+        repository.update(member.getMemberId(), updatedMoney);
+
+        Member updatedMember = repository.findById(member.getMemberId());
+
+        assertThat(updatedMember.getMoney()).isEqualTo(updatedMoney);
+
+        // Delete
+        repository.delete(member.getMemberId());
+
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
 }
