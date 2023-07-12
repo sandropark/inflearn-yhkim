69e52f7c76a6bf9cfce6911662fee187184c6ff1
sandropark <ticoba@naver.com>
2023-07-12 22:44:45 +0900

    스프링 핵심 원리 - 고급편 : 로그 추적기 V1 - 프로토타입 개발

---
+++ spring/core-advanced/src/main/java/com/sandro/trace/TraceStatus.java
@@ -0,0 +1,12 @@
+package com.sandro.trace;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TraceStatus {
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+}
