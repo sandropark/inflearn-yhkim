a66d6b3277e77cb9fbca93a9fb01f64b3e22a4be
sandropark <ticoba@naver.com>
2023-07-14 17:49:22 +0900

    스프링 핵심 원리 - 고급편 : 쓰레드 로컬 : 필드 동기화 - 적용

---
+++ spring/core-advanced/src/main/java/com/sandro/LogTraceConfig.java
@@ -0,0 +1,14 @@
+package com.sandro;
+
+import com.sandro.trace.logtrace.FieldLogTrace;
+import com.sandro.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+    @Bean
+    public LogTrace logTrace() {
+        return new FieldLogTrace();
+    }
+}
---
+++ spring/core-advanced/src/main/java/com/sandro/app/v3/OrderControllerV3.java
@@ -0,0 +1,29 @@
+package com.sandro.app.v3;
+
+import com.sandro.trace.TraceStatus;
+import com.sandro.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderControllerV3 {
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
