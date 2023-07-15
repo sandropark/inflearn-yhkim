134c60d3d80e7753297fa5012de2ab576fc76d6d
sandropark <ticoba@naver.com>
2023-07-15 14:39:59 +0900

    스프링 핵심 원리 - 고급편 : 쓰레드 로컬 : 쓰레드 로컬 동기화 - 적용

--- spring/core-advanced/src/main/java/com/sandro/LogTraceConfig.java
+++ spring/core-advanced/src/main/java/com/sandro/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.sandro;
 
-import com.sandro.trace.logtrace.FieldLogTrace;
 import com.sandro.trace.logtrace.LogTrace;
+import com.sandro.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class LogTraceConfig {
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
