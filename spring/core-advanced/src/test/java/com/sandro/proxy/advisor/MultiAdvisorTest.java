27e7f4e6772a022b82485c2207974aa4528baa8d
sandropark <ticoba@naver.com>
2023-07-21 05:45:59 +0900

    스프링 핵심 원리 - 고급편 : 스프링이 지원하는 프록시 - 여러 어드바이저 함께 적용

---
+++ spring/core-advanced/src/test/java/com/sandro/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,69 @@
+package com.sandro.proxy.advisor;
+
+import com.sandro.proxy.common.service.ServiceImpl;
+import com.sandro.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+class MultiAdvisorTest {
+
+    ServiceInterface target = new ServiceImpl();
+
+    @Test
+    void multiAdvisor1() throws Exception {
+        // client -> proxy2 -> proxy1 -> target
+
+        // 프록시1 생성
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advisor1());
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        // 프록시2 생성
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advisor2());
+        proxyFactory2.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        proxy2.save();
+    }
+
+    @Test
+    void multiAdvisor2() throws Exception {
+        // client -> proxy -> advisor1 -> advisor2 ->  target
+
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advisor1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advisor2());
+
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvisor(advisor2);
+        proxyFactory.addAdvisor(advisor1);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+    }
+
+    @Slf4j
+    static class Advisor1 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    @Slf4j
+    static class Advisor2 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+
+}
