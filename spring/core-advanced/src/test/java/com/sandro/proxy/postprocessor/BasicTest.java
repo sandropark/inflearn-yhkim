409ea80cdd2b07640d20620f06d6557b092a5652
sandropark <ticoba@naver.com>
2023-07-21 09:32:07 +0900

    스프링 핵심 원리 - 고급편 : 빈 후처리기 - 예제 코드

---
+++ spring/core-advanced/src/test/java/com/sandro/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,50 @@
+package com.sandro.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BasicTest {
+
+    private static final String BEAN_A = "beanA";
+
+    @Test
+    void basicConfig() throws Exception {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        A a = applicationContext.getBean(BEAN_A, A.class);
+        a.helloA();
+
+        assertThatThrownBy(() -> applicationContext.getBean(B.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+        @Bean(name = BEAN_A)
+        public A a() {
+            return new A();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+}
---
+++ spring/core-advanced/src/test/java/com/sandro/proxy/postprocessor/BeanPostProcessorTest.java
@@ -0,0 +1,68 @@
+package com.sandro.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BeanPostProcessorTest {
+
+    private static final String BEAN_A = "beanA";
+
+    @Test
+    void basicConfig() throws Exception {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
+
+        B b = applicationContext.getBean(BEAN_A, B.class);
+        b.helloB();
+
+        assertThatThrownBy(() -> applicationContext.getBean(A.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+    }
+
+    @Slf4j
+    @Configuration
+    static class BeanPostProcessorConfig {
+        @Bean(name = BEAN_A)
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public BeanPostProcessor aToBPostProcessor() {
+            return new AToBPostProcessor();
+        }
+    }
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+
+    @Slf4j
+    static class AToBPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={}, bean={}", beanName, bean);
+            if (bean instanceof A)
+                return new B();
+            return bean;
+        }
+    }
+}
