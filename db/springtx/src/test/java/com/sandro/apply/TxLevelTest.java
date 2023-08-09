b6315f29736149caaa5973afbef54c2ea342b08a
sandropark <ticoba@naver.com>
2023-08-10 06:41:06 +0900

    스프링 DB 2편 : 스프링 트랜잭션 이해 - 트랜잭션 적용 위치

---
+++ db/springtx/src/test/java/com/sandro/apply/TxLevelTest.java
@@ -0,0 +1,55 @@
+package com.sandro.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+class TxLevelTest {
+
+    @Autowired
+    LevelService service;
+
+    @Test
+    void order() throws Exception {
+        service.write();
+        service.read();
+    }
+
+    @SpringBootApplication
+    static class Context {
+        @Bean
+        public LevelService levelService() {
+            return new LevelService();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    static class LevelService {
+
+        @Transactional(readOnly = false)
+        public void write() {
+            log.info("call write");
+            printTxInfo();
+        }
+
+        public void read() {
+            log.info("call read");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly={}", readOnly);
+        }
+    }
+
+}
